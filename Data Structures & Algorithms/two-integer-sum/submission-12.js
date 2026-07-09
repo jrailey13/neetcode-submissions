a0fac7a4ba739db5702f99735e3bbf597260bf29
Jameson Railey <148562710+jrailey13@users.noreply.github.com>
2026-07-09 14:14:10 -0400

    Add: two-integer-sum - submission-12

---
+++ Data Structures & Algorithms/two-integer-sum/submission-12.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let valueArray = [];
+
+        for (let i = 0; i < nums.length; i++) {
+            let complement = target - nums[i];
+
+            if (valueArray.includes(nums[i])) return [valueArray.indexOf(nums[i]), i];
+
+            valueArray[i] = complement;
+            console.log(valueArray)
+        }
+
+        return valueArray;
+    }
+}
